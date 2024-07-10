dfd19ed6e69583e4badd5aaccf0c90fba8d647e3
Harsh Pandhe <pandhemihir@gmail.com>
2024-07-10 19:12:30 +0530

    Basic Working

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function checkPalindrome() {
+    const textInput = document.getElementById('textInput').value;
+    const cleanedText = textInput.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+    const reversedText = cleanedText.split('').reverse().join('');
+    const isPalindrome = cleanedText === reversedText;
+
+    document.getElementById('result').textContent = isPalindrome ? 'The text is a palindrome.' : 'The text is not a palindrome.';
+}
